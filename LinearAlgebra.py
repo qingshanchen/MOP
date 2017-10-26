973b88d5c1d718489eaa08fff618a940f294c066
Qingshan Chen <qingshan.chen@me.com>
2017-10-26 10:21:26 -0400

    LinearAlgebra.py: add ifs to check for situations where b is near zero, or where the initial guess is already close to the true solution.

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -3,20 +3,29 @@ import numpy as np
 
 def cg(A, b, x0, max_iter=1000, relres=1e-5):
 
-    counter = 0
     r = b - A.dot(x0)
     p = r.copy()
-    Ap = A.dot(p)
     r2 = np.dot(r,r)
     res0 = np.sqrt(np.sum(np.dot(b,b)))
-    
+    res = np.sqrt(np.sum(r2))
+
+    if res0 < np.finfo('float32').tiny:
+        # Zero right-hand; the solution should be zero
+        x0[:] = 0.
+        return 0, 0
+    elif res/res0 < relres:
+        # Initial guess close enough to the true solution
+        return 0, 0
+
+    counter = 0
     while True:
         # Punch the attendance card
         counter += 1
-        
-        alpha = r2 / np.dot(p, A.dot(p))
+
+        Ap = A.dot(p)
+        alpha = r2 / np.dot(p, Ap)
         x0 += alpha*p
-        r -= alpha*A.dot(p)
+        r -= alpha*Ap
         r2_new = np.dot(r,r)
         beta = r2_new / r2
         p = r + beta * p
