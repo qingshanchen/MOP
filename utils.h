24c7ab47509df6a936dffcfbe87edb7108af36a2
Qingshan Chen <qsc@atlantic.ces.clemson.edu>
2018-02-27 08:28:12 -0500

    Remove utils.c and utils.h

--- utils.h
+++
@@ -1,7 +0,0 @@
-double *alloc_1d_double(int);
-double **alloc_2d_double(int, int);
-int **alloc_2d_int(int, int);
-
-void free_1d_double(double *);
-void free_2d_double(double **);
-void free_2d_int(int **);
