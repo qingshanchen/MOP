11dbe3fca232621000e53db590598abb158f7c80
Qingshan Chen <qingshan.chen@me.com>
2018-06-21 15:24:06 -0400

    New branch -- hamilton to implement and test the scheme based on the Hamiltonian formulation.
    Trying to switch to the system supplied python and libraries.

--- build_fortran_module.sh
+++ build_fortran_module.sh
@@ -1,2 +1,3 @@
-/home/qsc/mylib/anaconda3/bin/f2py -c --f90flags='-ffree-line-length-none -fcheck=all' swe_comp.f90 -m swe_comp
+#/home/qsc/mylib/anaconda3/bin/f2py -c --f90flags='-ffree-line-length-none -fcheck=all' swe_comp.f90 -m swe_comp
+/usr/bin/f2py -c --f90flags='-ffree-line-length-none -fcheck=all' swe_comp.f90 -m swe_comp
 
