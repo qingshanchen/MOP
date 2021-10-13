Updated restart functionality for multilayer; fixed bug with multilayer total energy calculation on GPU.

Parameters.py
@@ -2,12 +2,12 @@ import numpy as np
 
 ### Parameters essential
 test_case = 2
-use_gpu = True
+use_gpu = False
 performing_test = False
 
 nLayers = 2
 vector_order = 'F'
-rho0 = 1
+rho0 = 1.0
 if use_gpu:
     import cupy as xp
 else:
@@ -39,7 +39,7 @@ no_slip_BC = False
 free_slip_BC = False
 
 # Solver config
-linear_solver = 'amgx'      # lu, cg, cudaCG, cudaPCG, amg, amgx
+linear_solver = 'lu'      # lu, cg, cudaCG, cudaPCG, amg, amgx
 err_tol = 5e-8
 max_iters = 1000
 print_stats = 0             # 1 for True, 0 for False
@@ -58,7 +58,7 @@ gravity = 9.80616
 output_file = 'output.nc'
 
 nTimeSteps = np.ceil(1.*86400*360/dt*nYears).astype('int')
-save_interval = 1e10#np.floor(1.*86400/dt*save_inter_days).astype('int')
+save_interval = np.floor(1.*86400/dt*save_inter_days).astype('int')
 if save_interval < 1:
     save_interval = 1
 